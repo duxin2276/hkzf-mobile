eb93f45c2b8465fa56a2eda3889e9758ce258a09
ddrrqq <ddrrqq@126.com>
2020-11-30 17:00:32 +0800

    获取轮播图数据。

--- src/pages/Home/Index/index.jsx
+++ src/pages/Home/Index/index.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import { Carousel } from 'antd-mobile'
 
 export default class Index extends Component {
+    state = {
+        carouselList: ['1', '2', '3']
+    }
+
+    async getCarouselList() {
+        const res = await (await fetch('http://127.0.0.1:8080/home/swiper')).json();
+
+        this.setState({ carouselList: res.body})
+    }
+
+    componentDidMount() {
+        this.getCarouselList();
+    }
+
+    renderCarousel() {
+        const { carouselList } = this.state;
+            
+        return (
+            <Carousel>
+                {carouselList.map(i => (
+                    <a>
+                        <img src="" alt=""/>
+                    </a>
+                ))}
+            </Carousel>
+        )
+    }
+
     render() {
         return (
-            <div>真正的首页</div>
+            <div>
+                {/* 轮播图区域 */}
+                
+            </div>
         )
     }
 }
