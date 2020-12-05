ce4b6c7378928a0f993783a9077576f87eb26061
ddrrqq <ddrrqq@126.com>
2020-12-05 16:56:34 +0800

    完成数据筛选展示。

--- src/pages/Home/HouseList/components/Filter/index.js
+++ src/pages/Home/HouseList/components/Filter/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import { API } from '../../../../../utils/api'
+import { Location } from '../../../../../utils/location'
+
 import FilterTitle from '../FilterTitle'
 import FilterPicker from '../FilterPicker'
 import FilterMore from '../FilterMore'
@@ -7,23 +10,68 @@ import FilterMore from '../FilterMore'
 import styles from './index.module.css'
 
 export default class Filter extends Component {
-  render() {
-    return (
-      <div className={styles.root}>
-        {/* 前三个菜单的遮罩层 */}
-        {/* <div className={styles.mask} /> */}
+    state = {
+        // 高亮状态（强行点亮+过去选中判断）。
+        titleStatus: 0,
+        filterData: undefined
+    }
 
-        <div className={styles.content}>
-          {/* 标题栏 */}
-          <FilterTitle />
+    get showPicker() {
+        return !!(this.state.titleStatus & 7)
+    }
 
-          {/* 前三个菜单对应的内容： */}
-          {/* <FilterPicker /> */}
+    get pickerDataSource() {
+        const { titleStatus, filterData: { area, subway, rentType, price } = {} } = this.state;
 
-          {/* 最后一个菜单对应的内容： */}
-          {/* <FilterMore /> */}
-        </div>
-      </div>
-    )
-  }
+        const dataCollection = {
+            1: [area, subway],
+            2: rentType,
+            4: price
+        };
+
+        return dataCollection[titleStatus];
+    }
+
+    async getFilterData() {
+        const res = await API.get('/houses/condition', { id: Location.getCity().value })
+        
+        console.log(res);
+
+        this.setState({ filterData: res.body })
+    }
+
+    componentDidMount() {
+        this.getFilterData()
+    }
+
+    changeStatusHandler(value) {
+        // 一半。
+        this.setState({ titleStatus: value})
+    }
+
+    onCancel() {
+        this.setState({ titleStatus: 0 })
+    }
+
+    render() {
+        const { titleStatus } = this.state
+
+        return (
+            <div className={styles.root}>
+                {/* 前三个菜单的遮罩层 */}
+                {this.showPicker && <div className={styles.mask} onClick={this.onCancel.bind(this)}/>}
+
+                <div className={styles.content}>
+                    {/* 标题栏 */}
+                    <FilterTitle changeStatus={this.changeStatusHandler.bind(this) } titleStatus={ titleStatus } />
+
+                    {/* 前三个菜单对应的内容： */}
+                    {this.showPicker && <FilterPicker dataSource={this.pickerDataSource} cols={titleStatus === 1 ? 3: 1 } onCancel={this.onCancel.bind(this)} /> }
+
+                    {/* 最后一个菜单对应的内容： */}
+                    {/* <FilterMore /> */}
+                </div>
+            </div>
+        )
+    }
 }
--- src/pages/Home/HouseList/components/FilterTitle/index.js
+++ src/pages/Home/HouseList/components/FilterTitle/index.js
@@ -6,22 +6,28 @@ import styles from './index.module.css'
 
 // 条件筛选栏标题数组：
 const titleList = [
-  { title: '区域', type: 'area' },
-  { title: '方式', type: 'mode' },
-  { title: '租金', type: 'price' },
-  { title: '筛选', type: 'more' }
+    { title: '区域', type: 'area' },
+    { title: '方式', type: 'mode' },
+    { title: '租金', type: 'price' },
+    { title: '筛选', type: 'more' }
 ]
 
-export default function FilterTitle() {
-  return (
-    <Flex align="center" className={styles.root}>
-      <Flex.Item>
-        {/* 选中类名： selected */}
-        <span className={[styles.dropdown, styles.selected].join(' ')}>
-          <span>区域</span>
-          <i className="iconfont icon-arrow" />
-        </span>
-      </Flex.Item>
-    </Flex>
-  )
+export default function FilterTitle({ titleStatus, changeStatus }) {
+    return (
+        <Flex align="center" className={styles.root}>
+            {titleList.map((i, idx) => {
+                const current = 1 << idx;
+                
+                return (
+                    <Flex.Item key={i.type} onClick={() => changeStatus(current)}>
+                        {/* 选中类名： selected */}
+                        <span className={[styles.dropdown, ...current & titleStatus ? [styles.selected] : []].join(' ')}>
+                            <span>{i.title}</span>
+                            <i className="iconfont icon-arrow" />
+                        </span>
+                    </Flex.Item>
+                )
+            })}
+        </Flex>
+    )
 }
