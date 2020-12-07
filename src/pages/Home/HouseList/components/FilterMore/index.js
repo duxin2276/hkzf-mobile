03a391f9cb541ff5bfde8d8c9c25411109e430cd
ddrrqq <ddrrqq@126.com>
2020-12-07 14:28:12 +0800

    实现手工复选框。

--- src/pages/Home/HouseList/components/FilterMore/index.js
+++ src/pages/Home/HouseList/components/FilterMore/index.js
@@ -5,40 +5,60 @@ import FilterFooter from '../../../../../components/FilterFooter'
 import styles from './index.module.css'
 
 export default class FilterMore extends Component {
-  // 渲染标签
-  renderFilters(list) {
-    return list.map(({ label, value }) => <span key={value} className={[styles.tag, styles.tagActive].join(' ')}>{label}</span>)
-    // 高亮类名： styles.tagActive
-  }
+    state = {
+        selectedValues: []
+    }
 
-  render() {
-    const { dataSource: { roomType, oriented, floor, characteristic } } = this.props
+    onTagClick(value) {
+        // 要么数组中有，就删除，要么没有就添加。
+        const { selectedValues: [...selectedValues] } = this.state
 
-    return (
-      <div className={styles.root}>
-        {/* 遮罩层 */}
-        <div className={styles.mask} />
+        selectedValues.some((i, idx, arr) => i === value && arr.splice(idx, 1)) || selectedValues.push(value);
 
-        {/* 条件内容 */}
-        <div className={styles.tags}>
-          <dl className={styles.dl}>
-            <dt className={styles.dt}>户型</dt>
-            <dd className={styles.dd}>{this.renderFilters(roomType)}</dd>
+        this.setState({ selectedValues });
+    }
 
-            <dt className={styles.dt}>朝向</dt>
-            <dd className={styles.dd}>{this.renderFilters(oriented)}</dd>
+    // 渲染标签
+    renderFilters(list) {
+        const { selectedValues } = this.state
 
-            <dt className={styles.dt}>楼层</dt>
-            <dd className={styles.dd}>{this.renderFilters(floor)}</dd>
+        return list.map(({ label, value }) => {
 
-            <dt className={styles.dt}>房屋亮点</dt>
-            <dd className={styles.dd}>{this.renderFilters(characteristic)}</dd>
-          </dl>
-        </div>
+            const isSelected = selectedValues.includes(value)
 
-        {/* 底部按钮 */}
-        <FilterFooter className={styles.footer} />
-      </div>
-    )
-  }
+            return <span key={value} onClick={this.onTagClick.bind(this, value)} className={[styles.tag, ...isSelected ? [styles.tagActive] : []].join(' ')}>{label}</span>
+        })
+        // 高亮类名： styles.tagActive
+    }
+
+    render() {
+        const { dataSource: { roomType, oriented, floor, characteristic } } = this.props
+
+        return (
+            <div className={styles.root}>
+                {/* 遮罩层 */}
+                <div className={styles.mask} />
+
+                {/* 条件内容 */}
+                <div className={styles.tags}>
+                    <dl className={styles.dl}>
+                        <dt className={styles.dt}>户型</dt>
+                        <dd className={styles.dd}>{this.renderFilters(roomType)}</dd>
+
+                        <dt className={styles.dt}>朝向</dt>
+                        <dd className={styles.dd}>{this.renderFilters(oriented)}</dd>
+
+                        <dt className={styles.dt}>楼层</dt>
+                        <dd className={styles.dd}>{this.renderFilters(floor)}</dd>
+
+                        <dt className={styles.dt}>房屋亮点</dt>
+                        <dd className={styles.dd}>{this.renderFilters(characteristic)}</dd>
+                    </dl>
+                </div>
+
+                {/* 底部按钮 */}
+                <FilterFooter className={styles.footer} />
+            </div>
+        )
+    }
 }
