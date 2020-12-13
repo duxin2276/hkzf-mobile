bf941aab61524e6695259b2b693265690a6f3368
ddrrqq <ddrrqq@126.com>
2020-12-13 16:33:25 +0800

    完整版。

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React, { Suspense, lazy, Component } from 'react'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { AuthRoute } from './components/AuthRoute'
 
 // 页面组件导入。
-import Home from './pages/Home'
-import CityList from './pages/CityList'
-import Map from './pages/Map'
-import HouseDetail from './pages/HouseDetail'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Rent from './pages/Rent'
-import RentAdd from './pages/Rent/Add'
-import RentSearch from './pages/Rent/Search'
-import { AuthRoute } from './components/AuthRoute'
+const Home          = lazy(() => import('./pages/Home'))
+const CityList      = lazy(() => import('./pages/CityList'))
+const Map           = lazy(() => import('./pages/Map'))
+const HouseDetail   = lazy(() => import('./pages/HouseDetail'))
+const Login         = lazy(() => import('./pages/Login'))
+const Register      = lazy(() => import('./pages/Register'))
+const Rent          = lazy(() => import('./pages/Rent'))
+const RentAdd       = lazy(() => import('./pages/Rent/Add'))
+const RentSearch    = lazy(() => import('./pages/Rent/Search'))
 
 import { Provider } from './store'
 
@@ -43,20 +43,22 @@ export default class App extends Component {
         return (
             <GlobalStore>
                 <Router>
-                    <div className="app">
-                        <Route path="/" exact render={() => <Redirect to="/home" />} />
-                        <Route path="/home" component={Home} />
-                        <Route path="/citylist" component={CityList} />
-                        <AuthRoute path="/map" component={Map} />
-                        <Route path="/detail/:id" component={HouseDetail} />
-                        <Route path="/login" component={Login} />
-                        <Route path="/register" component={Register} />
-                        <Route path="/rent" exact component={Rent} />
-                        <Route path="/rent/add" component={RentAdd} />
-                        <Route path="/rent/search" component={RentSearch} />
+                    <Suspense fallback={<div>Loading..</div>}>
+                        <div className="app">
+                            <Route path="/" exact render={() => <Redirect to="/home" />} />
+                            <Route path="/home" component={Home} />
+                            <Route path="/citylist" component={CityList} />
+                            <AuthRoute path="/map" component={Map} />
+                            <Route path="/detail/:id" component={HouseDetail} />
+                            <Route path="/login" component={Login} />
+                            <Route path="/register" component={Register} />
+                            <Route path="/rent" exact component={Rent} />
+                            <Route path="/rent/add" component={RentAdd} />
+                            <Route path="/rent/search" component={RentSearch} />
 
-                        <AuthRoute path="/vip" render={props => <h1>邀请码：6TGO341, {props.location.pathname}</h1>} />
-                    </div>
+                            <AuthRoute path="/vip" render={props => <h1>邀请码：6TGO341, {props.location.pathname}</h1>} />
+                        </div>
+                    </Suspense>
                 </Router>
             </GlobalStore>
         )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'antd-mobile/dist/antd-mobile.min.css'
+// import 'antd-mobile/dist/antd-mobile.min.css'
 import 'react-virtualized/styles.css'
 import './assets/fonts/iconfont.css'
 
